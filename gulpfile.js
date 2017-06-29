94dc297818bff88ba405267cb7e0f08872fa7bb9
Ksenia Polyakova <ksenia.v.polyakova@mail.ru>
2017-06-29 11:48:16 +0300

    настроен gulpfile: sass, pug, autoprefixer, jsconcat, watch

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+var gulp = require( 'gulp' ),
+	pug = require( 'gulp-pug' ),
+	sass = require( 'gulp-sass' ),
+	autopref = require('gulp-autoprefixer'),
+	concat = require( 'gulp-concat' ),
+	rename = require( 'gulp-rename' ),
+	sync = require( 'browser-sync' );
+
+gulp.task( 'pug', function() {
+	return gulp.src( 'src/**.pug' )
+	.pipe(pug())
+	.pipe(gulp.dest( 'app/' ))
+	} );
+
+gulp.task( 'sass', function() {
+	return gulp.src( 'src/**/**.sass' )
+	.pipe(sass())
+	.pipe(autoprefixer({
+		browsers: ['last 3 versions'],
+		cascade: false
+	}))
+	.pipe(gulp.dest( 'app/css/' ))
+	.pipe(sync.reload({ stream: true }))
+	} );
+
+gulp.task( 'jsconcat', function() {
+	return gulp.src( 'src/**/**.js' )
+	.pipe(concat('scripts.js'))
+	.pipe(gulp.dest( 'app/js/' ))
+	} );
+
+gulp.task( 'browserSync', function() {
+	browserSync({
+		server: {
+			baseDir: 'app'
+		},
+		notify: false
+		});
+	});
+
+gulp.task( 'watch', ['browserSync', 'sass', 'pug', 'jsconcat'], function() {
+	gulp.watch( 'src/**/**.**', ['pug', 'sass', 'jsconcat'] );
+});
+
+gulp.task('default', ['watch']);
