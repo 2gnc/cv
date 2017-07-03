e630b8cba2585a3c8419df4a6577c72d25025e66
Ksenia Polyakova <ksenia.v.polyakova@mail.ru>
2017-07-03 15:18:03 +0300

    незначительные изменения, правка комментариев

--- src/skill/skill.js
+++ src/skill/skill.js
@@ -1,18 +1,17 @@
-'use strict';
-// Функция для установки %% скиллов в скиллбар и заголовки
-	// находим скилл
+// Установка %% скиллов в скиллбар и заголовки
+// находим блок skill
 var skill = document.getElementsByClassName('skill');
 for (var i = skill.length-1; i >= 0; i-- ) {
-	// получаем значение скилла
+// получаем значение скилла
 	var percent = skill[i].querySelector( '.skill__name' ).getAttribute( 'data-percent' );
-	// для скилла получаем элемент - отображение скилла
+// для скилла получаем элемент - отображение скилла
 	var skillLine = skill[i].nextElementSibling.getElementsByClassName('skill__line-value');
-	//генерируем имя класса
+//генерируем имя класса
 	var classtoadd = 'skill__line-value--' + percent;
 // добавляем сгенерированный класс
 	skillLine[0].classList.add( classtoadd );
-	// добавляем правило в созданный класс
+// добавляем правило в созданный класс
 	document.styleSheets[2].addRule( '.' + classtoadd, ' width: ' + percent +'%' );
-	// Выводим процент на экран в skill__value
+// Выводим процент на экран в skill__value
 	skill[i].querySelector( '.skill__value' ).innerHTML = percent + '%';
 	};
